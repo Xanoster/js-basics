d21fb985b1e1e62e718a5cc6fa7554c28733d1ba
xanoster <your-email@example.com>
2024-08-28 21:08:41 +0530

    calculator

--- Scripts/10i-Calculator.js
+++ Scripts/10i-Calculator.js
@@ -1,7 +1,66 @@
-let input="";
-let operator=NULL;
-let result="";
+let output=""
+let curr=""
+let prev=""
+let operator=""
 
-function display(num){
-    
+function clickedNumber(num){
+switch(num){
+    case '+':
+        case '-':
+            case '*':
+                case '/':
+                    if(curr){
+                    prev=curr
+                    curr=""
+                    operator=num
+                    output+=operator
+                    }
+                    break;
+                    case '=':
+                        calculate(operator);
+                        break;
+                        default:
+                            curr+=num;
+                            output+=num;
+                            break;
+}
+display();
+}
+
+function calculate(op){
+    let result=0;
+    let p=parseFloat(prev)
+    let c=parseFloat(curr)
+switch (op){
+case '+':
+    result=p+c;
+    break;
+    case '-':
+        result=p-c;
+        break;
+        case '*':
+            result=p*c;
+            break;
+            case '/':
+                result=p/c;
+                break;
+                default:return;
+}
+output=result;
+prev=output;
+curr=""
+operator=""
+}
+
+function display(){
+    const a=document.querySelector('.show-clicked')
+    a.innerText=output;
+}
+function clearResults(){
+   output=""
+    curr=""
+    prev=""
+    operator=""
+    const b=document.querySelector('.show-clicked')
+    b.innerText= ""   
 }
