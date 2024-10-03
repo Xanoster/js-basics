49693e9ebf1ca708d50f62aea23a79a6581dff2c
xanoster <your-email@example.com>
2024-10-04 02:22:34 +0530

    todo-list-completed

--- Scripts/11-todo-list.js
+++ Scripts/11-todo-list.js
@@ -1,7 +1,40 @@
-const todoList=[];
+const todoList=[{
+    name:'Code for 30 minutes',
+    date:'10/3/2024'
+}];
+renderTodoList();
+
+function renderTodoList(){
+let todoListHTML='';
+for(let i=0;i<todoList.length;i++){
+    const todoObject=todoList[i];
+
+    //const name=todoObject.name;
+   // const date=todoObject.duedate;
+     
+   const {name,date}=todoObject;
+
+    const html=`<div>${name}</div>
+    <div> ${date} </div>
+    <button 
+    onclick="todoList.splice(${i},1);
+    renderTodoList();
+    " class='delete_button'>Delete</button> `;
+     todoListHTML+=html;
+}
+document.querySelector('.items').innerHTML=todoListHTML;
+}
 
 function addTodo(){
-const item=document.querySelector(".input-box")
-todoList.push(item.value);
-console.log(todoList);
+    const nameElement=document.querySelector(".input-box")
+    const name=nameElement.value;
+    const dateElement=document.querySelector(".date")
+    const date=dateElement.value;
+    todoList.push({
+        name,date
+    });
+    console.log(todoList)
+    nameElement.value='';
+    renderTodoList();
 }
+
