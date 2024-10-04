8f1cd612ad0508aa46d0a27eb75efb4a03f72bfc
xanoster <your-email@example.com>
2024-10-04 23:42:23 +0530

    todo project

--- Scripts/11-todo-list.js
+++ Scripts/11-todo-list.js
@@ -1,40 +1,35 @@
-const todoList=[{
-    name:'Code for 30 minutes',
-    date:'10/3/2024'
-}];
+const todoList=JSON.parse(localStorage.getItem('todoList')) || [];
+
 renderTodoList();
 
-function renderTodoList(){
-let todoListHTML='';
-for(let i=0;i<todoList.length;i++){
-    const todoObject=todoList[i];
+function renderTodoList() {
+    let todoListHTML = '';
+    for (let i = 0; i < todoList.length; i++) {
+        const todoObject = todoList[i];
+        const { name, date } = todoObject;
 
-    //const name=todoObject.name;
-   // const date=todoObject.duedate;
-     
-   const {name,date}=todoObject;
-
-    const html=`<div>${name}</div>
-    <div> ${date} </div>
-    <button 
-    onclick="todoList.splice(${i},1);
-    renderTodoList();
-    " class='delete_button'>Delete</button> `;
-     todoListHTML+=html;
-}
-document.querySelector('.items').innerHTML=todoListHTML;
+        const html = `<div>${name}</div>
+        <div> ${date} </div>
+        <button 
+        onclick="deleteTodo(${i})" class='delete_button'>Delete</button>`;
+        todoListHTML += html;
+    }
+    document.querySelector('.items').innerHTML = todoListHTML;
 }
 
-function addTodo(){
-    const nameElement=document.querySelector(".input-box")
-    const name=nameElement.value;
-    const dateElement=document.querySelector(".date")
-    const date=dateElement.value;
-    todoList.push({
-        name,date
-    });
-    console.log(todoList)
-    nameElement.value='';
+function addTodo() {
+    const nameElement = document.querySelector(".input-box");
+    const name = nameElement.value;
+    const dateElement = document.querySelector(".date");
+    const date = dateElement.value;
+    todoList.push({ name, date });
+    localStorage.setItem('todoList', JSON.stringify(todoList)); 
+    nameElement.value = '';
     renderTodoList();
 }
 
+function deleteTodo(index) {
+    todoList.splice(index, 1);
+    localStorage.setItem('todoList', JSON.stringify(todoList)); 
+    renderTodoList();
+}
